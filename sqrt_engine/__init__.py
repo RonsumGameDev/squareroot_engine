41876756d96ce6c0fb6d02f4dbc4d582f4192e9a
RonsumGameDev <ronakdwivedi69@gmail.com>
2025-06-20 00:54:32 +0530

    sqrt_engine __init__.py

---
+++ sqrt_engine/__init__.py
@@ -0,0 +1,10 @@
+"""
+Sqrt Engine by Saksham 'Ronsum' Dwivedi
+The Ultimate Digit-by-Digit Square Root Engine. This uses the classic Long division method of calculating square roots.
+"""
+
+from .core import sqroot
+
+__all__ = ["sqroot"]
+__version__ = "1.0.0"
+__author__ = "Saksham 'Ronsum' Dwivedi"
